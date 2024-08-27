917249e403b8cb3bb8833b8ec209d6dc8593debe
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2024-08-27 23:54:58 +0200

    añadiendo tipos de variables

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,13 @@
+//tipos de variables
+var disponible = 0
+let disponible1;
+const disponible2 = 0;
+
+/**
+*JS es un lenguaje sin tipado, por ende una variable puede ser 
+*de varios tipos [int, String, boolean, char, etc.. ]
+**/
+
+
+
+
