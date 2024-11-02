592423d18464e95ce89f7928ed3f31055d891cae
DavidKuguk <46162029+Davidkuguk@users.noreply.github.com>
2024-11-02 13:55:54 +0100

    ok

--- 13-DOM/js/07-scripts.js
+++ 13-DOM/js/07-scripts.js
@@ -4,3 +4,10 @@ h1.style.color = 'red;';
 h1.style.textTransform = 'uppercase';
 
 const card = document.querySelector('.card');
+
+const nav = document.querySelector('nav');
+nav.style.fontWeight = '900'
+
+nav.classList.add('nueva-clase');
+const hover = document.querySelector('.nueva-clase:hover');
+hover.style.backgroundColor = 'red';
